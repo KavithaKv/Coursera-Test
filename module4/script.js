59950d7d99890279e1b59def16dfea265a0848ce
KavithaKv <118251316+KavithaKv@users.noreply.github.com>
2022-12-30 09:47:20 +0530

    Update script.js

--- module4/script.js
+++ module4/script.js
@@ -49,7 +49,7 @@ for (var name in names) {
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
   var firstLetter = name.toString().chartAt(0).toLowercase();
-
+  console.log(firstLetter);
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
